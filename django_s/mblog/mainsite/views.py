4d8c0d502c8fe618c2cd5fa5ed057d05d6fd7cbd
xuanwu348 <xuanwu348@163.com>
2018-11-07 16:45:09 +0000

    a tag

--- django_s/mblog/mainsite/views.py
+++ django_s/mblog/mainsite/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template.loader import get_template
 from .models import Post
@@ -20,3 +20,21 @@ def homepage(request):
     #html = template.render(locals())
     html = template.render({"posts":posts, "now":now})
     return HttpResponse(html)
+
+def showpost(request, title):
+    template = get_template("post.html")
+    try:
+        """
+        posts = Post.objects.all()
+        for post in posts:
+            if post.title == title:
+                html = template.render({"post":post})
+                return HttpResponse(html)
+        """
+        post = Post.objects.get(title=title)
+        if post is not None:
+            html = template.render({"post":post})
+            return HttpResponse(html)
+    except:
+        return redirect("/")
+    
