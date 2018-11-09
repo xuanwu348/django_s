ae47acfa2859f7f8a82f1698bc27de5bcd58a8aa
xuanwu348 <xuanwu348@163.com>
2018-11-09 16:59:28 +0000

    add template and html

--- django_s/ww45/mysite/views.py
+++ django_s/ww45/mysite/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.template.loader import get_template
+from django.http import HttpResponse
+from mysite import models
 
 # Create your views here.
 
 def index(request):
-    pass
+    product = models.Product.objects.all()
+    template = get_template("index.html")
+    html = template.render({"product":product})
+    return HttpResponse(html)
