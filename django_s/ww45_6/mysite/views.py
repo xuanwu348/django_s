e8b54fde343310478fb1b195bf32969577bbff26
xuanwu348 <xuanwu348@163.com>
2018-11-10 16:17:03 +0000

    update

--- django_s/ww45_6/mysite/views.py
+++ django_s/ww45_6/mysite/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.loader import get_template
 
 # Create your views here.
+def index(request):
+    template = get_template('index.html')
+    try:
+        urid = request.GET['user_id']
+        urpass = request.GET["user_pass"]
+        byear = request.GET['byear']
+        urfcolor = request.GET.getlist("fcolor")
+    except:
+        urid = None
+    if urid is not None and len(urid) >=1 and urpass == "12345":
+        verified = True
+    else:
+        verified = False
+    years = range(1960,2021)
+    html = template.render(locals())
+    return HttpResponse(html)
+
+    """
+    the first display
+    template = get_template("index.html")
+    return render(request=request, template_name="index.html")
+    """
--- django_s/ww45_6/ww45_6/settings.py
+++ django_s/ww45_6/ww45_6/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p$#q&s1==%(p&$s^zpf#!ez+!lhb&wcz^w+7q#4e5=$a9@llo)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mysite',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'ww45_6.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+        ]
