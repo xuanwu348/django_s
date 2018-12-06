07cf0ca55af2cb5b6b57d646bc283e8f00acea7f
Your Name <you@example.com>
2018-12-07 01:20:47 +0800

    tutoria7

--- django_API_REST_BASE/site_002/app001/urls.py
+++ django_API_REST_BASE/site_002/app001/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
 from app001 import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
+schema_view = get_schema_view(title="Pastebin API")
 router = DefaultRouter()
 router.register(r'app001s', views.App001ViewSet)
 router.register(r'users', views.UserViewSet)
 
-urlpatterns = [path("", include(router.urls)),]
+urlpatterns = [path("", include(router.urls)),
+               path("schema/", schema_view),
+              ]
 #urlpatterns = format_suffix_patterns(urlpatterns)
